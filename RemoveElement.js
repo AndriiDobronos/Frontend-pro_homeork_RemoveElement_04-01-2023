fa4ded80356c6d6915966cfbef50160a521ae7af
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-04 20:43:15 +0200

    added variant

--- RemoveElement.js
+++ RemoveElement.js
@@ -1,6 +1,7 @@
 
 //Реалізуйте функцію removeElement(array, item),
 // щоб видалити елемент item з масиву array.
+
 const array = [1, 2, 3, 4, 5, 6, 7];
 function removeElement(arr, b) {
     arr.splice((b - 1),1)
@@ -10,4 +11,9 @@ function removeElement(arr, b) {
 const b = 5
 let result = removeElement(array,b)
 alert(result);
-// Результат: [1, 2, 3, 4, 6, 7]
+// Результат: [1, 2, 3, 4, 6, 7] */
+const arras = [1, 2, 3, 4, 5, 6, 7];
+const filterArr = arras.filter(function(el,index,arr) {
+    return el !== 5
+})
+alert(filterArr )
