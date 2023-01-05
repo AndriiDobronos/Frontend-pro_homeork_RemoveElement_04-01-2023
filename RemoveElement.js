e0f712cc097d4e15a1a28df849eefe5edfe10587
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-05 11:16:57 +0200

    corrected homework

--- RemoveElement.js
+++ RemoveElement.js
@@ -2,15 +2,15 @@
 //Реалізуйте функцію removeElement(array, item),
 // щоб видалити елемент item з масиву array.
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-function removeElement(arr, b) {
-    arr.splice((b - 1),1)
-    return array
+//const array = [1, 2, 3, 4, 5, 6, 7];
+//function removeElement(arr, b) {
+//    arr.splice((b - 1),1)
+//    return array
 //    return String(arr).split(b).join('')  /* alternative */
-}
-const b = 5
-let result = removeElement(array,b)
-alert(result);
+//}
+//const b = 5
+//let result = removeElement(array,b)
+//alert(result);
 // Результат: [1, 2, 3, 4, 6, 7] */
 const arras = [1, 2, 3, 4, 5, 6, 7];
 const filterArr = arras.filter(function(el,index,arr) {
