da5ed7e28404215701e3b51cdbbab3e6323d08b3
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-07 23:15:01 +0200

    enhanced homework

--- RemoveElement.js
+++ RemoveElement.js
@@ -1,19 +1,15 @@
-
+"use strict";
 //Реалізуйте функцію removeElement(array, item),
 // щоб видалити елемент item з масиву array.
+// Результат: [1, 2, 3, 4, 6, 7]
 
-//const array = [1, 2, 3, 4, 5, 6, 7];
-//function removeElement(arr, b) {
-//    arr.splice((b - 1),1)
-//    return array
-//    return String(arr).split(b).join('')  /* alternative */
-//}
-//const b = 5
-//let result = removeElement(array,b)
-//alert(result);
-// Результат: [1, 2, 3, 4, 6, 7] */
 const arras = [1, 2, 3, 4, 5, 6, 7];
-const filterArr = arras.filter(function(el,index,arr) {
-    return el !== 5
-})
-alert(filterArr )
+const element = 5
+
+function removeElement(array, item) {
+    return array.filter
+    (function (el, index, arr) {
+        return el !== item
+    })
+}
+alert(removeElement(arras, element))
